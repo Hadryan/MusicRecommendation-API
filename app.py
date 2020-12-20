6c3bb3b6a1e34ae68af8515d761d1a1f26d43e29
Daffa <daffamaulana30@gmail.com>
2020-12-21 03:15:16 +0800

    fix

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import model
 
 app = Flask(__name__)
-CORS(app) 
-@app.route('/music/', methods=['GET'])
+CORS(app)
 
-def recommend_movies():
+
+@app.route('/music/', methods=['GET'])
+def music():
     res = model.results(request.args.get('title'))
     # print(res)
     return jsonify(res)
 
-if __name__=='__main__':
-    app.run(port = 5000, debug = True)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
